8ae5a793099fc37d6f6d75f39053e5446bd1770f
AbhijeetHellan <108428901+AbhijeetHellan@users.noreply.github.com>
2025-09-22 22:11:04 -0400

    Empty Implementations for Process API
    * Implementations for Process API
    
    Checkpoint 3
    
    * Updated ProcessAPIIMPL
    
    added intereger stream with a default faliure

---
+++ src/emptyimplementations/IntegerStreamImpl.java
@@ -0,0 +1,14 @@
+package emptyimplementations;
+
+import java.util.List;
+
+import processapi.IntegerStream;
+
+public class IntegerStreamImpl implements IntegerStream {
+
+	@Override
+	public List<Integer> getNumbers() {
+		return null;
+	}
+
+}
---
+++ src/emptyimplementations/ProcessAPIImpl.java
@@ -0,0 +1,30 @@
+package emptyimplementations;
+
+import processapi.DataStorageAPI;
+import processapi.IntegerStream;
+import processapi.ReadRequest;
+import processapi.ReadResponse;
+import processapi.WriteRequest;
+import processapi.WriteResponse;
+
+/*
+ An empty implementation of the DataStorageAPI 
+ This class handles reading from and writing to a data source,
+*/
+public class ProcessAPIImpl implements DataStorageAPI {
+
+	// Reads data from a source.
+	@Override
+	public ReadResponse readData(ReadRequest request) {
+		// Return a default failure response with an empty stream to avoid errors.
+		IntegerStream emptyStream = new IntegerStreamImpl();
+		return new ReadResponseImpl();
+	}
+
+	// Writes data to a destination.
+	@Override
+	public WriteResponse writeData(WriteRequest request) {
+		// Return a default failure response as the logic is not implemented.
+		return new WriteResponseImpl();
+	}
+}
