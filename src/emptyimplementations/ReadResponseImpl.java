f53290c519493ee845cdf2c55fb44dab5db462cd
AbhijeetHellan <108428901+AbhijeetHellan@users.noreply.github.com>
2025-09-24 19:27:02 -0400

    Infrastructure for In-Memory Implementations
    Checkpoint 3  .  Also updated ProcessAPI Implementation to make InMemory Implementations for Data Store API.

--- src/emptyimplementations/ReadResponseImpl.java
+++ src/emptyimplementations/ReadResponseImpl.java
@@ -4,15 +4,22 @@ import processapi.IntegerStream;
 import processapi.ReadResponse;
 
 public class ReadResponseImpl implements ReadResponse {
+    private final boolean success;
+    private final IntegerStream stream;
 
-	@Override
-	public boolean isSuccess() {
-		return false;
-	}
+    // Add this constructor
+    public ReadResponseImpl(boolean success, IntegerStream stream) {
+        this.success = success;
+        this.stream = stream;
+    }
 
-	@Override
-	public IntegerStream getIntegerStream() {
-		return null;
-	}
+    @Override
+    public boolean isSuccess() {
+        return this.success;
+    }
 
+    @Override
+    public IntegerStream getIntegerStream() {
+        return this.stream;
+    }
 }
