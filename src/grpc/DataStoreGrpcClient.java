55aee06d58aaa85e1c59f7a14368c87015ce1587
AbhijeetHellan <108428901+AbhijeetHellan@users.noreply.github.com>
2025-12-04 12:06:46 -0500

    Performance Testing and Optimization
    * Performance Testing and Optimization
    
    * Update OptimizedConceptualAPIImpl.java

--- src/grpc/DataStoreGrpcClient.java
+++ src/grpc/DataStoreGrpcClient.java
@@ -22,7 +22,10 @@ class DataStoreGrpcClient implements DataStorageAPI {
 	private final DataStoreServiceGrpc.DataStoreServiceBlockingStub stub;
 
 	public DataStoreGrpcClient(String host, int port) {
-		this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+		this.channel = ManagedChannelBuilder.forAddress(host, port)
+	            .usePlaintext()
+	            .maxInboundMessageSize(64 * 1024 * 1024) // updated to allow up to 64 MB input
+	            .build();
 		this.stub = DataStoreServiceGrpc.newBlockingStub(channel);
 		System.out.println("  Connected to Data Store at " + host + ":" + port);
 	}
